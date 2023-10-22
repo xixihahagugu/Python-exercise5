8162cab905688d191e53252508ce8b069e7d70b5
xixihahagugu <Mengna.Deng@metropolia.fi>
2023-10-22 21:54:44 +0800

    5.3

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # Exercises Module 5.3
 
-cities = []
+num = int(input("Enter a number to check if it is a prime number: "))
 
-for i in range(5):
-    city = input("Enter a city name: ")
-    cities.append(city)
+if num > 1:
+    is_prime = True
+    for i in range(2, int(num/2) + 1):
+        if (num % i) == 0:
+            is_prime = False
+            break
 
-print("List of cities:")
-for city in cities:
-    print (city)
+    if is_prime:
+        print(num, "is a prime number")
+
+    else:
+        print(num, "is not a prime number")
+
+else:
+    print(num, "is not a prime number")
